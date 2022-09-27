d2dddb9ad9a200738b69e0df0b7d73eb393fc579
Miss-Melissa <94617701+Miss-Melissa@users.noreply.github.com>
2022-09-27 10:41:18 +0200

    Nyheter sidan
    lagt till kod

---
+++ themes/fimero/template_parts/post-template.php
@@ -0,0 +1,15 @@
+<div>
+    <div>
+        <a href="http://localhost:8888/wp-grupp?p=<?= get_the_id(); ?>">
+
+
+
+    </div>
+    <div>
+        <h3><?= the_title(); ?></h3>
+        <b><?= the_date(); ?></b>
+        <p><?= the_content(); ?></p>
+    </div>
+
+    </a>
+</div>
