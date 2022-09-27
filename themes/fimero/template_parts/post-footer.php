b46525977bdae5f10a90465b5854a60ce154ca4d
rollyHub <rolly.farias@medieinstitutet.se>
2022-09-27 13:28:02 +0200

    Footer
    logo/butiksnamn

--- themes/fimero/template_parts/post-footer.php
+++ themes/fimero/template_parts/post-footer.php
@@ -1,9 +1,21 @@
 <footer>
 
-<small class="footer-text-color">FiMeRo<?= date("Y"); ?>. All rights reserved </small>
+<?php
+    $site_title = get_bloginfo('name');
+    $site_url = network_site_url('/')
+    ?>
+
+<small class="footer-text-color">Information private policy term agreement afiliation<?= date("Y"); ?>. All rights reserved </small>
 
 <div class="menu-style">
             <?php wp_nav_menu(array('theme_location' => "my-footer-menu")); ?>
         </div>
+
+        <div class="header-style">
+        <a href="<?php echo $site_url ?>" class="logo">
+            <h3 class="logo-h3">
+                <?php echo $site_title; ?>
+            </h3>
+        </a>
 
 </footer>
