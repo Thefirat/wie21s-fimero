7da606af55b0808f9033d210cd47f71c148a1271
Miss-Melissa <94617701+Miss-Melissa@users.noreply.github.com>
2022-09-27 10:28:08 +0200

    Seach.php
    håller på och fixar search

--- themes/fimero/index.php
+++ themes/fimero/index.php
@@ -4,4 +4,8 @@ if (have_posts()) : while (have_posts()) : the_post();
     get_template_part('entry');
   endwhile;
 endif;
+?>
+
+
+<?php
 get_footer();
--- themes/fimero/template_parts/post-block.php
+++ themes/fimero/template_parts/post-block.php
@@ -1,4 +1,19 @@
-<div class="post-block">
-    <h2><?= the_title(); ?></h2>
-    <p><?= the_date(); ?></p>
-</div>
+<?php $the_query = new WP_Query(array(
+    'posts_per_page' => 4,
+));
+?>
+
+<?php if ($the_query->have_posts()) : ?>
+    <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+        <a href="http://localhost:8888/wp-grupp?p=<?= get_the_id(); ?>">
+
+            <?= the_title(); ?>
+            <?= the_date(); ?>
+            <?= the_excerpt(); ?>
+
+        </a>
+
+    <?php endwhile; ?>
+    <?php wp_reset_postdata(); ?>
+<?php else : ?>
+<?php endif; ?>
