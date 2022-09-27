933e745a058ef04c53a3c94f522233c62549145b
Miss-Melissa <94617701+Miss-Melissa@users.noreply.github.com>
2022-09-27 12:07:40 +0200

    Fixes in functions.php
    :)

--- themes/fimero/functions.php
+++ themes/fimero/functions.php
@@ -3,7 +3,6 @@
 function wpb_custom_new_menu()
 {
     register_nav_menu('my-custom-menu', __('My Custom Menu'));
-    register_nav_menu('my-footer-menu', __('My Footer Menu'));
 }
 
 add_action('init', 'wpb_custom_new_menu');
@@ -16,4 +15,3 @@ function add_woocommerce_support()
 }
 
 add_action('after_setup_theme', 'add_woocommerce_support');
-
