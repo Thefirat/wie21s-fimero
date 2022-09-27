5004890d03eb5b44547f0485a03ebebe1bc66539
Miss-Melissa <94617701+Miss-Melissa@users.noreply.github.com>
2022-09-27 14:26:13 +0200

    footer.php
    fixes

--- themes/fimero/template_parts/post-footer.php
+++ themes/fimero/template_parts/post-footer.php
@@ -1,24 +1,28 @@
 <footer>
 
-<?php
+    <?php
     $site_title = get_bloginfo('name');
     $site_url = network_site_url('/')
     ?>
 
-<small class="footer-text-color">Information private policy term agreement afiliation<?= date("Y"); ?>. All rights reserved </small>
-
-<div class="menu-style">
-    <h3>NAVIGATE</h3>
-    <h4>INFORMATION</h4>
+    <small class="footer-text-color">Information private policy term agreement afiliation<?= date("Y"); ?>. All rights reserved </small>
+    <div>
+        <div>
+            <h3>NAVIGATE</h3>
             <?php wp_nav_menu(array('theme_location' => "my-footer-menu")); ?>
+        </div>
+        <div>
+            <h4>INFORMATION</h4>
             <?php wp_nav_menu(array('theme_location' => "my-second-footer-menu")); ?>
         </div>
 
-        <div class="header-style">
-        <a href="<?php echo $site_url ?>" class="logo">
-            <h3 class="logo-h3">
-                <?php echo $site_title; ?>
-            </h3>
-        </a>
 
+        <div>
+            <a href="<?php echo $site_url ?>" class="logo">
+                <h3 class="logo-h3">
+                    <?php echo $site_title; ?>
+                </h3>
+            </a>
+        </div>
+    </div>
 </footer>
