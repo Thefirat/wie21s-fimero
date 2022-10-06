f2c356271d4fd1f00772cd439cbc4032533e8d09
Firat Akkoyun <92083856+Thefirat@users.noreply.github.com>
2022-10-06 14:44:47 +0200

    added klarna

--- themes/fimero/template_parts/post-template-index.php
+++ themes/fimero/template_parts/post-template-index.php
@@ -5,7 +5,7 @@
         <div class="container">
             <h3 class="h3-posts"><b><?= the_title(); ?></b></h3>
             <p class="p-posts"><?= the_content(); ?></p>
-            <a class="a-posts" href="http://localhost:8888/wp-grupp?p=<?= get_the_id(); ?>">LÄS MER</a>
+            <a class="a-posts" href="http://localhost:8888/wp-grupp/wordpress?p=<?= get_the_id(); ?>">LÄS MER</a>
         </div>
     </div>
 
